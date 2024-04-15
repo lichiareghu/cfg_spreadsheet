730760220f8457c75457165f3a7ece2c79d9ba90
lichiareghu <90550296+lichiareghu@users.noreply.github.com>
2024-04-15 18:40:30 +0100

    updating get profile

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
-from functions import read_data, analyse_data
+from functions import (read_data,
+                       analyse_data,
+                       percentage_change,
+                       get_profile)
 
 
 def main():
@@ -13,44 +16,79 @@ def main():
         # Read the filename into a dict
         data = read_data(f'data_files/{uploaded_file.name}')
 
-        # Convert the data to a dataframe
-        df = pd.DataFrame(data)
+        # Read the data to a dataframe
+        df = pd.read_csv(f'data_files/{uploaded_file.name}')
 
-        # Display the sample data
-        st.subheader("Sample:")
-        st.dataframe(df.head(3))
+        # Generate profile
+        profile = get_profile(df)
+
+        # Convert the dict in to dataframe
+        profile_df = pd.DataFrame(profile)
+
+        # Display the data profile
+        st.subheader("Profile:")
+        st.dataframe(profile_df)
 
         # Split the layout into two columns
         col1, col2 = st.columns(2)
 
         # Inputs column
         with col1:
-            st.header("Inputs")
+            st.header("Data Analysis")
 
             # Create a dropdown button with options
-            values_column = st.selectbox("Select the values column", df.columns)
-            # grouping_column = st.selectbox("Select the grouping column", df.columns)
+            options = df.columns
+            values_column = st.selectbox("Select the values column", options)
+            grouping_column = st.selectbox("Select the grouping column", options, index=None)
             # partition_column = st.selectbox("Select the partition column", df.columns)
 
             # Define the operations
             operations = ['Total', 'Average', 'Median', 'Minimum', 'Maximum']
-            #operation = st.selectbox("Select the operation", operations)
+            # operation = st.selectbox("Select the operation", operations)
             operation = st.multiselect("Select the operation", operations)
 
             # Create a button
             button_clicked = st.button("Calculate")
 
-        # Outputs column (aligned to the right)
+        # Outputs column
         with col2:
             st.header("Outputs")
             if button_clicked:
-                result_dict = analyse_data(data, values_column, operation)
+
+                result = analyse_data(data, values_column, operation, grouping_column)
 
                 # Convert the results into dataframe
-                result_df = pd.DataFrame(result_dict.items(), columns=['Operation', 'Results'])
+                result_df = pd.DataFrame(result, columns=['Operation', 'Group',values_column.capitalize()])
 
-                st.write("Data Analysis")
+                # st.write("Data Analysis")
                 st.dataframe(result_df)
+
+        # Display percentage change
+        st.subheader("Percentage change")
+        # Create a button
+        button_clicked2 = st.button("Get % Change")
+        if button_clicked2:
+            df['change'] = percentage_change(data, values_column)
+            st.dataframe(df[[grouping_column, values_column, 'change']])
+
+        # Display the graph
+        st.subheader("Plot Graph")
+
+        # Create a dropdown button with options
+        # graph_options = st.selectbox("Select the type of graph", ['Percentage change', 'Simple Plot'])
+
+        # if graph_options == 'Simple Plot':
+        x = st.selectbox("Choose X axis", df.columns)
+        y = st.selectbox("Choose Y axis", df.columns)
+        z = st.selectbox("Choose colour", df.columns)
+        button_clicked3 = st.button("Plot")
+        if button_clicked3:
+            st.scatter_chart(
+                    df,
+                    x=x,
+                    y=y,
+                    color=z
+                )
 
 
 if __name__ == "__main__":
--- functions.py
+++ functions.py
@@ -14,36 +14,67 @@ def read_data(file):
     return data
 
 
-def collect_data(data, values):
+def convert_values(data, values_column):
+    """Takes an input dict, converts the value to numbers saves the new value and returns the dict
+    inputs: data -> 'Dict'
+            values_column -> 'list'
+    output: 'Dict''"""
+    for row in data:
+        row[values_column] = float(row[values_column])
+    return data
+
+
+def collect_data(data, value_column, grouping_column=None):
     """Collect the values from a column and return it
     inputs: data-> dict of keys and values
-            values-> 'str' type key in data to be used for collecting values
-    outputs: collected_data-> 'list' with numeric values"""
-    collected_data = []
-    for row in data:
-        value = row[values]
-        collected_data.append(float(value))
+            value_column-> 'str' type key in data to be used for collecting values
+            grouping_column-> 'str' type key to be used for grouping the data to be collected
+    outputs: collected_data-> 'dict' with group names as keys and numeric values"""
+    collected_data = {}
+    df = pd.DataFrame(data)
+    df[value_column] = [float(i) for i in df[value_column]]
+    if grouping_column:
+        groups = set(df[grouping_column])
+        for group in groups:
+            grp = df.loc[df[grouping_column] == group, value_column].tolist()
+            collected_data[group] = grp
+    else:
+        collected_data[value_column] = df[value_column].tolist()
     return collected_data
 
 
-def analyse_data(data, values_column, operation):
+def analyse_data(data, values_column, operation, grouping_column=None):
     """Complete analysis and return result
     inputs: data-> dict of column names and values
             values_column-> 'str' type name of the column to be used for numerical calculations
             operation-> 'list' type value representing the names of the operation
-    outputs: result_data-> Dict with keys as operation and results as values"""
+            grouping_column-> 'str' type key to be used for grouping the data to be collected
+    outputs: result_data-> list of lists arranged in the order [operation,group,result]"""
     # Initialise the result dict
-    result_data = {}
+    result_data = []
 
     # Get the results as list
-    workable_data = collect_data(data, values_column)
+    workable_data = collect_data(data, values_column, grouping_column)
 
     # Iterate through the list of operations and get the results
     for op in operation:
-        result_data[op] = get_result(workable_data, op)
+        for group, data in workable_data.items():
+            result = get_result(data, op)
+            result_data.append([op, group, result])
 
     # Return the data
     return result_data
+
+
+def collect_numerical(data):
+    # collect only float and int values which are not null
+    tmp = []
+    for item in data:
+        if isinstance(item, int) or isinstance(item, float):
+            tmp.append(item)
+        else:
+            tmp.append(0)
+    return tmp
 
 
 def get_result(values, operation):
@@ -54,6 +85,7 @@ def get_result(values, operation):
     """
 
     result = 0
+    values = collect_numerical(values)
 
     if operation == 'Total':
         result = total(values)  # Write a function to add all values
@@ -74,23 +106,28 @@ def total(data):
     input : list of values
     output : int or float
     """
-    return sum(data)
+    total = 0
+    for i in range(len(data)):
+        total = total + data[i]
+    return total
+
 
 def average(data):
     # input: list of values
     # output: float
-    return sum(data)/len(data)
+    return round(sum(data) / len(data), 2)
+
 
 def median(data):
-     """input: list of values
-        output: float
-     """
+    """input: list of values
+            output: float
+        """
     sorted_data = sorted(data)
-    lenght = len(sorted_data)
-    if lenght % 2==0:
-        return (sorted_data[lenght//2])
-
-
+    length = len(sorted_data)
+    if length % 2 == 0:
+        return round((sorted_data[length // 2 - 1] + sorted_data[length // 2]) / 2, 2)
+    else:
+        return round(sorted_data[length // 2], 2)
 
 
 def minimum(data):
@@ -111,3 +148,47 @@ def maximum(data):
     return max(data)
 
 
+def percentage_change(data, values_column):
+    """
+    This function takes a dict of data and a grouping column and a value column. It then returns the percentage change
+    input :
+    data: dict of values
+    grouping_column: str
+    values_column: str
+    output : dict of values
+    """
+    list_changes = [0]
+    data = convert_values(data, values_column)
+    for i in range(1, len(data)):
+        previous_month = float(data[i - 1][values_column])
+        current_month = float(data[i][values_column])
+        if previous_month and previous_month != 0:
+            change_as_percentage = round(((current_month - previous_month) / previous_month) * 100, 2)
+        else:
+            change_as_percentage = 0
+        list_changes.append(change_as_percentage)
+    return list_changes
+
+
+def get_profile(df):
+    """
+    This function collects the data in a dataframe and generates a profile for the data
+    :param df: Input data frame
+    :return: Dict of summary
+    """
+    df_numeric = pd.DataFrame(columns=df.columns)
+    for i in df.columns:
+        df_numeric[i] = collect_numerical(df[i].tolist())
+    result = {'Columns': [i for i in df.columns],
+              'Data Type': [df[i].dtype.name for i in df],
+              'No of values': [len(df[i].dropna()) for i in df],
+              'No of missing values': [df[i].isna().sum() for i in df],
+              'No of unique values': [len(df[i].unique()) for i in df],
+              'Maximum value': [maximum(df_numeric[i].tolist()) for i in df],
+              'Minimum value': [minimum(df_numeric[i].tolist()) for i in df],
+              'Mean': [round(df[df[i].notna()][i].mean(), 2) if df[i].dtype.name != 'object' else 0 for i in df],
+              'Std_dev': [round(df[df[i].notna()][i].std(), 2) if df[i].dtype.name != 'object' else 0 for i in df]
+              # ,'Quantile': [df[df[i].notna()][i].quantile([.25, .5, .75])
+              # if df[i].dtype.name != 'object' else 0 for i in df]
+              }
+    return result
